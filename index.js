cf8b202c10c4102a5f78426b7416345e7f3fc305
Rumatoid <skripnikovanton.r@gmail.com>
2020-02-21 18:34:07 +0300

    1

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 const router = require('./routes/router');
 app.use(router);
 
-mongoose.connect('mongodb://localhost/test', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
